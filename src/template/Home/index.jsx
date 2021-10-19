6a778bf988fe8d38d75148115690c25edf617497
Lucas-yago <41453379+Lucas-yago@users.noreply.github.com>
2021-10-19 15:11:01 -0300

    Migra componente Home para função

--- src/template/Home/index.jsx
+++ src/template/Home/index.jsx
@@ -1,5 +1,6 @@
 import './style.css';
-import { Component } from 'react';
+// import { Component } from 'react';
+import { useEffect, useState, useCallback} from 'react';
 
 import { loadPosts } from '../../utils/load-posts'
 import { Posts } from '../../components/Posts';
@@ -7,106 +8,198 @@ import { Button } from '../../components/Button';
 import { SearchBar } from '../../components/SearchBar'
 
 
-export class Home extends Component {
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 12,
-    searchValue: '',
-    loading: false
-  };
+export const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(12);
+  const [searchValue, setSearchValue] = useState('');
+  const [loading, setLoading] = useState(false);
 
-
-  componentDidMount() {
-    document.title = 'Aprendendo-React';
-    this.loadPosts();
-  }
-
-
-
-
-  loadPosts = async () => {
-    const { page, postsPerPage } = this.state;
-    this.setState({ loading: true })
-    
-    const postsAndPhotos = await loadPosts();
-    this.setState({
-      ...this.state,
-      posts: postsAndPhotos.slice(page, postsPerPage),
-      allPosts: postsAndPhotos,
-      loading: false
-
+  const noMorePosts = (page + postsPerPage) >= allPosts.length;
+  const filterPosts = !!searchValue ? (
+    allPosts.filter(post => {
+      return post.title.toLowerCase().includes(
+        searchValue.toLowerCase()
+      );
     })
-  }
+  ) : posts;
 
-  loadMorePosts = () => {
-    const {
-      posts,
-      allPosts,
-      page,
-      postsPerPage
-    } = this.state;
+  const handleLoadPosts = useCallback( async (page, postsPerPage) => {
+
+    setLoading(true);
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+    setLoading(false);
+  },[])
+
+  const handleloadMorePosts = () => {
 
     const nextPage = (page + postsPerPage);
     const nextPost = allPosts.slice(nextPage, nextPage + postsPerPage)
     posts.push(...nextPost)
 
-    this.setState({ posts, page: nextPage });
+    setPosts(posts);
+    setPage(nextPage);
   }
 
-  handleChange = (e) => {
+  const handleChange = (e) => {
     const { value } = e.target;
 
-    this.setState({ searchValue: value });
-
+    setSearchValue(value);
   }
 
-
-  render() {
-    const { posts, page, postsPerPage, allPosts, searchValue, loading } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-
-
-    const filterPosts = !!searchValue ? (
-      allPosts.filter(post => {
-        return post.title.toLowerCase().includes(
-          searchValue.toLowerCase()
-        );
-      })
-    ) : posts;
-
-    return (
-      <section className="container">
-        <div className='container-SearchBar'>
-          {!!searchValue && (<h1>Buscar por: {searchValue}</h1>)}
-
-          <SearchBar
-            handleChange={this.handleChange}
-            searchValue={searchValue}
-          />
-        </div>
+  useEffect(() => {
+    handleLoadPosts(0, postsPerPage);
+  },[handleLoadPosts, postsPerPage])
 
 
-        {filterPosts.length > 0 && (
-          <Posts posts={filterPosts} />
-        )}
 
 
-        {filterPosts.length === 0 && !loading  && (
-          <h2>Nenhuma busca encontrada 😔</h2>
-        )}
+  
+
+ 
 
 
-        {!searchValue && !loading&& (
-          <Button text='Load More Posts'
-            onClick={this.loadMorePosts}
-            disabled={noMorePosts}
-          />
-        )}
+  return (
+    <section className="container">
+      <div className='container-SearchBar'>
+        {!!searchValue && (<h1>Buscar por: {searchValue}</h1>)}
 
-      </section>
-    );
-  }
+        <SearchBar
+          handleChange={handleChange}
+          searchValue={searchValue}
+        />
+      </div>
+
+
+      {filterPosts.length > 0 && (
+        <Posts posts={filterPosts} />
+      )}
+
+
+      {filterPosts.length === 0 && !loading && (
+        <h2>Nenhuma busca encontrada 😔</h2>
+      )}
+
+
+      {!searchValue && !loading && (
+        <Button text='Load More Posts'
+          onClick={handleloadMorePosts}
+          disabled={noMorePosts}
+        />
+      )}
+
+    </section>
+  );
+
+
 };
+
+// Comentado apenas para fins de estudo e comparação
+
+// export class Home extends Component {
+//   state = {
+//     posts: [],
+//     allPosts: [],
+//     page: 0,
+//     postsPerPage: 12,
+//     searchValue: '',
+//     loading: false
+//   };
+
+
+//   componentDidMount() {
+//     document.title = 'Aprendendo-React';
+//     this.loadPosts();
+//   }
+
+
+
+
+//   loadPosts = async () => {
+//     const { page, postsPerPage } = this.state;
+//     this.setState({ loading: true })
+
+//     const postsAndPhotos = await loadPosts();
+//     this.setState({
+//       ...this.state,
+//       posts: postsAndPhotos.slice(page, postsPerPage),
+//       allPosts: postsAndPhotos,
+//       loading: false
+
+//     })
+//   }
+
+//   loadMorePosts = () => {
+//     const {
+//       posts,
+//       allPosts,
+//       page,
+//       postsPerPage
+//     } = this.state;
+
+//     const nextPage = (page + postsPerPage);
+//     const nextPost = allPosts.slice(nextPage, nextPage + postsPerPage)
+//     posts.push(...nextPost)
+
+//     this.setState({ posts, page: nextPage });
+//   }
+
+//   handleChange = (e) => {
+//     const { value } = e.target;
+
+//     this.setState({ searchValue: value });
+
+//   }
+
+
+//   render() {
+//     const { posts, page, postsPerPage, allPosts, searchValue, loading } = this.state;
+//     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+
+//     const filterPosts = !!searchValue ? (
+//       allPosts.filter(post => {
+//         return post.title.toLowerCase().includes(
+//           searchValue.toLowerCase()
+//         );
+//       })
+//     ) : posts;
+
+//     return (
+//       <section className="container">
+//         <div className='container-SearchBar'>
+//           {!!searchValue && (<h1>Buscar por: {searchValue}</h1>)}
+
+//           <SearchBar
+//             handleChange={this.handleChange}
+//             searchValue={searchValue}
+//           />
+//         </div>
+
+
+//         {filterPosts.length > 0 && (
+//           <Posts posts={filterPosts} />
+//         )}
+
+
+//         {filterPosts.length === 0 && !loading  && (
+//           <h2>Nenhuma busca encontrada 😔</h2>
+//         )}
+
+
+//         {!searchValue && !loading&& (
+//           <Button text='Load More Posts'
+//             onClick={this.loadMorePosts}
+//             disabled={noMorePosts}
+//           />
+//         )}
+
+//       </section>
+//     );
+//   }
+// };
 
